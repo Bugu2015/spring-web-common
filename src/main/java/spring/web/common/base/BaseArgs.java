ac3ec2c6b2d0915b6fb3f71a06b6fb601790e00e
Cywen <857990246@qq.com>
2019-08-07 12:22:19 +0800

    push args

--- src/main/java/spring/web/common/base/BaseArgs.java
+++ src/main/java/spring/web/common/base/BaseArgs.java
@@ -4,5 +4,14 @@ import lombok.Data;
 
 @Data
 public class BaseArgs {
-
+    private String uuid;
+    private String phoneType;
+    private String netType;
+    private String latLng;
+    private String gps;
+    private String appVersion;
+    private String sysVersion;
+    private String userToken;
+    private String time;
+    private String sign;
 }
