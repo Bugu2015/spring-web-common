4e9b543721efb504d85664a296c43bc99e9b8113
Cywen <857990246@qq.com>
2019-08-08 13:41:08 +0800

    push exception

--- src/main/java/spring/web/common/exception/NormalException.java
+++ src/main/java/spring/web/common/exception/NormalException.java
@@ -1,11 +1,20 @@
 package spring.web.common.exception;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import spring.web.common.enums.BaseEnum;
 
 public class NormalException extends BaseException {
 
+    private static final Log log = LogFactory.getLog(NormalException.class);
+
     public NormalException(BaseEnum baseEnum) {
         super(baseEnum);
+    }
+
+    public NormalException(BaseEnum baseEnum, Exception e) {
+        super(baseEnum);
+        log.debug(e);
     }
 
 }
--- src/main/java/spring/web/common/exception/UnKnowException.java
+++ src/main/java/spring/web/common/exception/UnKnowException.java
@@ -1,11 +1,20 @@
 package spring.web.common.exception;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import spring.web.common.enums.BaseEnum;
 
 public class UnKnowException extends BaseException {
 
+    private static final Log log = LogFactory.getLog(NormalException.class);
+
     public UnKnowException(BaseEnum baseEnum) {
         super(baseEnum);
+    }
+
+    public UnKnowException(BaseEnum baseEnum, Exception e) {
+        super(baseEnum);
+        log.info(e);
     }
 
 }
